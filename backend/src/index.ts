1e1ee12dd4c5ae00de964b0c20dc40aeadd0c6ae
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-05-27 17:10:17 +0200

    Update dockerfile Frontend and Backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 6868;
+const port = parseInt(process.env.PORT || '', 10) || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen( port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()
+    ,tailwindcss()
+  ],
+  server: {
+    host: true, // Automatyczne wykrywanie hosta
+    port: 3000, 
+    strictPort: true,
+    hmr: {
+      port: 3000, 
+      clientPort: 3000 
+    },
+    watch: {
+      usePolling: true
+    }
+  }
+});
