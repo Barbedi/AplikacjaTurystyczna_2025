571e3aea1bc6476e859674599fb4b8b82fa9e0d9
Bartłomiej Bednarek <146706805+Barbedi@users.noreply.github.com>
2025-06-04 00:34:34 +0200

    AT-3 Update navbar file

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,27 +1,34 @@
 const Navbar = () => {
   return (
     <div>
-      {/* Sekcja z tłem */}
       <div className="h-screen bg-[url('/assets/img/FullSizeRender2.JPG')] bg-cover bg-center bg-no-repeat">
         <nav className="bg-nav bg-opacity-70 font-lora p-4">
           <div className="flex">
             <a href="/" className="pl-4">
-              <span className="font-bold text-4xl">HikeUp</span>
+              <span className="font-bold text-white text-4xl">HikeUp</span>
             </a>
+            <div className="flex-grow"></div>
+            <div className="flex items-center space-x-4 pr-4">
+              <a href="/plan-route" className="text-white text-xl hover:text-gray-300 transition duration-300 hover:border-2 rounded-2xl p-1">
+                Zaplanuj trase
+              </a>
+              <a href="/login" className="text-white text-xl hover:text-gray-300 transition duration-300  hover:border-2 rounded-2xl p-1">
+                Zaloguj się
+              </a>
+              </div>
           </div>
         </nav>
-        <div className="flex flex-col items-center justify-center bg-white/60 max-w-2/3 mx-auto max-h-2/4 rounded-lg shadow-lg mt-20 p-8">
-          <div className="text-black text-center flex flex-col items-center">
+        <div className="flex flex-col items-center justify-center bg-white/60 md:max-w-2/3 md:mx-auto md:max-h-2/4 lg:max-w-2/4 lg:min-h-3/6 rounded-lg shadow-lg lg:mt-44 md:mt-20 p-8">
+          <div className="text-black text-center flex flex-col items-center ">
             <h1 className="text-6xl font-lora">Wędrówka marzeń?</h1>
             <p className="text-6xl font-lora">Znajdziesz ją z HikeUp!</p>
           </div>
-
-          <div className="mt-8 flex flex-col items-center w-full">
-            <div className="mb-4 w-full flex justify-center">
+          <div className="mt-10 flex flex-col items-center w-full">
+            <div className="mb-4 w-full flex justify-center ">
               <input
                 type="text"
                 placeholder="Wyszukaj szlaku"
-                className="w-2/4 p-2 border border-gray-300 rounded-2xl"
+                className="w-2/4 p-2 border-2  border-black rounded-2xl hover:border-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 text-lg font-lora text-black"
               />
             </div>
             <div className="mb-4">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 import "./index.css";
-import Home from "./pages/Home";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
