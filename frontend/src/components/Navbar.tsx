78ffbbf5a728f6099e8a3c9b151052254b2ec3a5
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-06-03 21:19:21 +0200

    AT-3 Add img and start created navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,88 +1,38 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartSimple } from "@fortawesome/free-solid-svg-icons/faChartSimple";
-import { faFile } from "@fortawesome/free-solid-svg-icons/faFile";
-import { faGamepad } from "@fortawesome/free-solid-svg-icons/faGamepad";
-
 const Navbar = () => {
   return (
-    <nav className="bg-nav font-lora p-4">
-      <div className="container mx-auto flex items-center">
-        <a href="/" className="flex items-center">
-          <img
-            src="/android-chrome-192x192.png"
-            alt="Logo firmy"
-            className="h-12 w-auto"
-            width={120}
-            height={48}
-          />
-          <span className="md:hidden text-white font-lora mx-4 items-center gap-[3vw] text-5xl max-[340px]:text-3xl">
-            NeuroControl
-          </span>
-        </a>
-        <div className="hidden md:flex mx-4 items-center">
-          <ul className="flex gap-8">
-            <li>
-              <a
-                href="/"
-                className="text-black font-jersey text-3xl hover:text-gray-200"
-              >
-                Monitorowanie
-              </a>
-            </li>
-            <li>
-              <a
-                href="/sterowanie"
-                className="text-black font-jersey text-3xl hover:text-gray-200"
-              >
-                Sterowanie
-              </a>
-            </li>
-            <li>
-              <a
-                href="/raporty"
-                className="text-black font-jersey text-3xl hover:text-gray-200"
-              >
-                Raporty
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="md:hidden fixed bottom-0 left-0 z-50 w-full h-20 bg-nav">
-          <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
-            <a
-              href="/"
-              className="inline-flex flex-col items-center justify-center px-5"
-            >
-              <FontAwesomeIcon
-                icon={faChartSimple}
-                className="text-3xl text-white"
-              />
-              <span className="text-sm text-white font-jersey">
-                Monitorowanie
-              </span>
-            </a>
-            <a
-              href="/sterowanie"
-              className="inline-flex flex-col items-center justify-center px-5 "
-            >
-              <FontAwesomeIcon
-                icon={faGamepad}
-                className="text-3xl text-white"
-              />
-              <span className="text-sm text-white font-jersey">Sterowanie</span>
-            </a>
-
-            <a
-              href="/raporty"
-              className="inline-flex  flex-col items-center justify-center px-5 "
-            >
-              <FontAwesomeIcon icon={faFile} className="text-3xl text-white" />
-              <span className="text-sm text-white font-jersey">Raporty</span>
+    <div>
+      {/* Sekcja z tłem */}
+      <div className="h-screen bg-[url('/assets/img/FullSizeRender2.JPG')] bg-cover bg-center bg-no-repeat">
+        <nav className="bg-nav bg-opacity-70 font-lora p-4">
+          <div className="flex">
+            <a href="/" className="pl-4">
+              <span className="font-bold text-4xl">HikeUp</span>
             </a>
           </div>
-        </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center bg-white/60 max-w-2/3 mx-auto max-h-2/4 rounded-lg shadow-lg mt-20 p-8">
+  <div className="text-black text-center flex flex-col items-center">
+    <h1 className="text-6xl font-lora">Wędrówka marzeń?</h1>
+    <p className="text-6xl font-lora">Znajdziesz ją z HikeUp!</p>
+  </div>
+
+  <div className="mt-8 flex flex-col items-center w-full">
+    <div className="mb-4 w-full flex justify-center">
+      <input
+        type="text"
+        placeholder="Wyszukaj szlaku"
+        className="w-2/4 p-2 border border-gray-300 rounded-2xl"
+      />
+    </div>
+    <div className="mb-4">
+      <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+        Szukaj
+      </button>
+    </div>
+  </div>
+</div>
       </div>
-    </nav>
+    </div>
   );
 };
 
