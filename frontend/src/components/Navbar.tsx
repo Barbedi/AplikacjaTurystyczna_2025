6f400faa5250dc914969122d78129fde28d6d9d6
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-06-06 20:52:01 +0200

    AT-10 Update navbar and fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const images = [
-  '/assets/img/FullSizeRender.JPG',
-  '/assets/img/FullSizeRender2.JPG',
-  '/assets/img/IMG_4048.JPG',
+  "/assets/img/FullSizeRender.JPG",
+  "/assets/img/FullSizeRender2.JPG",
+  "/assets/img/IMG_4048.JPG",
 ];
-
 
 const Navbar = () => {
   const [current, setCurrent] = useState(0);
@@ -15,54 +14,47 @@ const Navbar = () => {
   const next = () => setCurrent((prev) => (prev + 1) % images.length);
   const prev = () => setCurrent((prev) => (prev - 1 + images.length) % images.length);
 
-
   useEffect(() => {
-  const interval = setInterval(() => {
-    setCurrent((prev) => (prev + 1) % images.length);
-  }, 5000);
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 7000);
 
-  return () => clearInterval(interval);
-}, []);
-
-
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="relative w-full h-screen overflow-hidden">
-      <img
-        src={images[current]}
-        className="w-full h-full object-cover transition-all duration-500 ease-in-out z-0"
-      />
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
-          {images.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrent(index)}
-              className={`w-3 h-3 rounded-full transition-all duration-300 ${
-                current === index ? 'bg-white scale-125' : 'bg-gray-300'
-              }`}
-              aria-current={current === index ? 'true' : 'false'}
-              aria-label={`Slide ${index + 1}`}
-            />
-          ))}
-        </div>
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className="w-full h-screen object-cover flex-shrink-0"
+            alt={`Slide ${index}`}
+          />
+        ))}
+      </div>
       <button
         onClick={prev}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full z-10 hover:scale-105 cursor-pointer transition duration-300"
-       >
+        className="absolute z-30 top-1/2 left-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full  hover:scale-105 cursor-pointer transition duration-300"
+      >
         <FontAwesomeIcon icon={faArrowLeft} className="text-xl" />
       </button>
       <button
         onClick={next}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full z-10"
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full z-30 hover:scale-105 cursor-pointer transition duration-300"
       >
         <FontAwesomeIcon icon={faArrowRight} className="text-xl" />
       </button>
-      <header className="absolute top-0 left-0 w-full bg-nav bg-opacity-70 font-lora p-4 z-10">
+      <header className="absolute top-0 left-0 w-full bg-nav bg-opacity-70 font-lora p-4 z-30">
         <div className="flex items-center justify-between">
-          <a href="/" className="pl-4">
+          <a href="/" className="pl-4 ">
             <span className="font-bold text-white text-4xl">HikeUp</span>
           </a>
-          <div className="flex items-center space-x-4 pr-4">
+          <div className="flex items-center space-x-4 pr-4 z-0">
             <a
               href="/plan-route"
               className="text-white text-xl hover:text-gray-300 transition duration-300 hover:border-2 rounded-2xl p-1"
@@ -80,8 +72,12 @@ const Navbar = () => {
       </header>
       <main className="absolute inset-0 flex items-center justify-center z-10 p-4">
         <div className="bg-white/60 rounded-lg shadow-lg p-8 max-w-2xl w-full text-center">
-          <h1 className="text-5xl md:text-6xl font-lora text-black mb-4">Wędrówka marzeń?</h1>
-          <p className="text-4xl md:text-5xl font-lora text-black mb-8">Znajdziesz ją z HikeUp!</p>
+          <h1 className="text-5xl md:text-6xl font-lora text-black mb-4">
+            Wędrówka marzeń?
+          </h1>
+          <p className="text-4xl md:text-5xl font-lora text-black mb-8">
+            Znajdziesz ją z HikeUp!
+          </p>
 
           <div className="flex flex-col items-center">
             <input
--- frontend/src/components/PlanRoute.tsx
+++ frontend/src/components/PlanRoute.tsx
@@ -3,24 +3,35 @@ const PlanRoute = () => {
     <div className="w-full flex items-center justify-center bg-gray-100">
       <div className="flex flex-col items-center justify-center w-full max-w-screen-xl bg-secondary mx-4 my-10 py-6 px-4 rounded-3xl">
         <div className="flex flex-col items-center justify-center mt-4">
-          <span className="text-3xl sm:text-4xl text-white font-lora">Zaplanuj trasę</span>
+          <span className="text-3xl sm:text-4xl text-white font-lora">
+            Zaplanuj trasę
+          </span>
         </div>
 
         <div className="flex flex-row mt-6">
           <div className="flex flex-row lg:flex-nowrap flex-wrap justify-center gap-4 mt-6">
             <div className="flex flex-col w-full sm:w-1/2 xl:w-1/5 h-auto bg-white rounded-xl p-3 mx-1 hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
               <div className="flex flex-col items-center text-center space-y-2">
-                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">1</h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">Wybierz punkt startowy</h2>
+                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">
+                  1
+                </h1>
+                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">
+                  Wybierz punkt startowy
+                </h2>
                 <span className="text-sm sm:text-base md:text-lg font-lora">
-                  Kliknij na mapie miejsce lub wpisz miejsce, z którego chcesz wyruszyć.
+                  Kliknij na mapie miejsce lub wpisz miejsce, z którego chcesz
+                  wyruszyć.
                 </span>
               </div>
             </div>
             <div className="flex flex-col w-full sm:w-1/2 xl:w-1/5 h-auto bg-white rounded-xl p-3 mx-1 hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
               <div className="flex flex-col items-center text-center space-y-2">
-                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">2</h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">Dodaj punkt docelowy</h2>
+                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">
+                  2
+                </h1>
+                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">
+                  Dodaj punkt docelowy
+                </h2>
                 <span className="text-sm sm:text-base md:text-lg font-lora">
                   Wybierz miejsce, do którego chcesz dojść.
                 </span>
@@ -28,8 +39,12 @@ const PlanRoute = () => {
             </div>
             <div className="flex flex-col w-full sm:w-1/2 xl:w-1/5 h-auto bg-white rounded-xl p-3 mx-1 hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
               <div className="flex flex-col items-center text-center space-y-2">
-                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">3</h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">Dodaj punkty pośrednie (opcjonalnie)</h2>
+                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">
+                  3
+                </h1>
+                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">
+                  Dodaj punkty pośrednie (opcjonalnie)
+                </h2>
                 <span className="text-sm sm:text-base md:text-lg font-lora">
                   Jeśli chcesz, dodaj przystanki lub ciekawe miejsca po drodze.
                 </span>
@@ -37,14 +52,17 @@ const PlanRoute = () => {
             </div>
             <div className="flex flex-col w-full sm:w-1/2 xl:w-1/5 h-auto bg-white rounded-xl p-3 mx-1 hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
               <div className="flex flex-col items-center text-center space-y-2">
-                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">4</h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">Zatwierdź trasę i zapisz plan</h2>
+                <h1 className="text-2xl sm:text-3xl md:text-5xl font-lora">
+                  4
+                </h1>
+                <h2 className="text-lg sm:text-xl md:text-2xl font-lora">
+                  Zatwierdź trasę i zapisz plan
+                </h2>
                 <span className="text-sm sm:text-base md:text-lg font-lora">
                   Gotowe? Zapisz trasę i ruszaj na wyprawę z HikeUp!
                 </span>
               </div>
             </div>
-
           </div>
         </div>
 
