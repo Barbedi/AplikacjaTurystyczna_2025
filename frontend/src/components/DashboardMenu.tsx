ce08579543c652545e99e18f5d8af1853ac378bb
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-06-27 21:25:10 +0200

    AT-16 Improve DashboardMenu responsiveness and layout

--- frontend/src/components/DashboardMenu.tsx
+++ frontend/src/components/DashboardMenu.tsx
@@ -18,8 +18,8 @@ const DashboardMenu = () => {
     });
   }, []);
   return (
-    <div className="flex flex-col items-start justify-between h-screen w-80 border-r-2 border-white bg-transparent/30 relative">
-      <div className="w-full p-4 overflow-y-auto">
+    <div className="flex flex-col items-start justify-between h-screen md:w-70 w-80 border-r-2 border-white bg-transparent/30 relative">
+      <div className="flex-1 w-full p-4 overflow-hidden">
         {/* Profil */}
         <div className="flex items-center">
           <img
@@ -27,75 +27,71 @@ const DashboardMenu = () => {
             src="/assets/img/FullSizeRender.JPG"
             alt="user"
           />
-          <h2 className="text-white text-lg font-bold ml-4">{user?.email}</h2>
+          <h2 className="text-white md:text-sm 2xl:text-lg font-bold ml-4">{user?.email}</h2>
         </div>
 
         {/* Menu */}
-        <div className="flex flex-col mt-6 w-full space-y-2">
-          <span className="text-white text-lg font-lora py-2 px-4 text-left">
+        <div className="flex flex-col md:mt-2  mt-6 w-full space-y-2 md:space-y-1">
+          <span className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left">
             DASHBOARDS
           </span>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Zaplanuj trase
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Wyszukaj szlaku
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Moje trasy
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Ulubione trasy
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Proponowane trasy
           </button>
         </div>
-        <div className="flex flex-col mt-6 w-full space-y-2">
-          <span className="text-white text-lg font-lora py-2 px-4 text-left">
+        <div className="flex flex-col md:mt-2  mt-6 w-full md:space-y-1 space-y-2">
+          <span className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left">
           Postępy
           </span>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Moje szczyty
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
-            Korona Gór Polski
+            Korony Gór
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
-            <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
-            Korona Gór Beskidu Sadeckeigo
-          </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Statystyki
           </button>
         </div>
-        <div className="flex flex-col mt-6 w-full space-y-2">
-          <span className="text-white text-lg font-lora py-2 px-4 text-left">
+        <div className="flex flex-col md:mt-2  mt-6 w-full md:space-y-1 space-y-2">
+          <span className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left">
             Ustawienia
           </span>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Moj profil
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Moje opinie
           </button>
-          <button className="text-white text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
+          <button className="text-white md:text-sm 2xl:text-lg font-lora py-2 px-4 text-left rounded-2xl hover:bg-gray-700 transition duration-300">
             <FontAwesomeIcon icon={faChevronRight} className="mr-2 text-sm" />
             Wyloguj
           </button>
         </div>
       </div>
-      <div className="w-full py-6">
+      <div className="w-full md:py-3 py-6">
         <Link to="/" className="block text-center text-white text-4xl font-bold">
           HikeUp
         </Link>
