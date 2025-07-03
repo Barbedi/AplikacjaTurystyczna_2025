ee6b00c74218a10905457993bf339391540c7401
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-07-03 11:13:02 +0200

    AT-27 Add reviews component and update manager pages

--- frontend/src/pages/Manager/MyReviews.tsx
+++ frontend/src/pages/Manager/MyReviews.tsx
@@ -1,10 +1,11 @@
+import Reviews from "../../components/Manager/Reviews";
+
 const MyReviews = () => {
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl text-white font-bold mb-4">Moje recenzje</h1>
-      <p className="text-gray-500 text-lg">
-        Ta strona jest jeszcze w budowie. Wkrótce będzie dostępna!
-      </p>
+    <div className="flex flex-col text-center mx-6 gap-2 w-full justify-center items-center my-4">
+      <h1 className="text-3xl font-semibold text-white ">Moje opinie</h1>
+      <Reviews />
+      <Reviews />
     </div>
   );
 };
