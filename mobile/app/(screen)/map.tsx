12504331258e991dd9142f7d5fed6ebe2d9b2341
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-11-14 19:36:11 +0100

    Refactor map screen and add index redirect

--- mobile/app/(screen)/map.tsx
+++ mobile/app/(screen)/map.tsx
@@ -1,20 +1,31 @@
+import React from "react";
 import { View, StyleSheet } from "react-native";
-import { MapView, Camera } from "@maplibre/maplibre-react-native";
+import  MapLibreGL from "@maplibre/maplibre-react-native";
 
 export default function MapScreen() {
-  const MAPTILER_KEY = "TWÓJ_KEY";
+  const MAPTILER_KEY = "DdJo20VMMy7tFRXLTfO6"; // twój klucz
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        mapStyle={`https://api.maptiler.com/maps/outdoor-v2/style.json?key=${MAPTILER_KEY}`}
-      >
-        <Camera
+      <MapLibreGL.MapView style={styles.map}>
+        <MapLibreGL.Camera
           zoomLevel={10}
-          centerCoordinate={[19.94, 50.06]}
+          centerCoordinate={[19.94, 50.06]} // Kraków
         />
-      </MapView>
+
+        <MapLibreGL.RasterSource
+          id="maptiler-outdoor"
+          tileSize={256}
+          tileUrlTemplates={[
+            `https://api.maptiler.com/maps/outdoor-v2/{z}/{x}/{y}.png?key=${MAPTILER_KEY}`,
+          ]}
+        >
+          <MapLibreGL.RasterLayer
+            id="maptiler-outdoor-layer"
+            sourceID="maptiler-outdoor"
+          />
+        </MapLibreGL.RasterSource>
+      </MapLibreGL.MapView>
     </View>
   );
 }
