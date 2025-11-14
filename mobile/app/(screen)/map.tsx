c7045fa0f4fccb68895d48c7f6a3c917be75ca7c
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2025-11-14 17:46:03 +0100

    Refactor map screen and update dependencies

--- mobile/app/(screen)/map.tsx
+++ mobile/app/(screen)/map.tsx
@@ -1,41 +1,25 @@
-import { StyleSheet, View } from 'react-native';
-
+import { View, StyleSheet } from "react-native";
 import { MapView, Camera } from "@maplibre/maplibre-react-native";
 
-export default function App() {
-
-  const MAPTILER_API_KEY = "DdJo20VMMy7tFRXLTfO6";
+export default function MapScreen() {
+  const MAPTILER_KEY = "TWÓJ_KEY";
 
   return (
     <View style={styles.container}>
-      <View style={styles.mapcontainer}>
-        <MapView 
-          style={styles.map} 
-          mapStyle={`https://api.maptiler.com/maps/outdoor-v2/{z}/{x}/{y}.png?key=DdJo20VMMy7tFRXLTfO6`}
-          logoEnabled={false}
-          attributionPosition={{bottom: 8, right: 8}}>
-            <Camera
-  centerCoordinate={[2, 41.5]}
-  zoomLevel={8}
-/>
-        </MapView>
-      </View>
+      <MapView
+        style={styles.map}
+        mapStyle={`https://api.maptiler.com/maps/outdoor-v2/style.json?key=${MAPTILER_KEY}`}
+      >
+        <Camera
+          zoomLevel={10}
+          centerCoordinate={[19.94, 50.06]}
+        />
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapcontainer: {
-    width: '100%',
-    height: '100%',
-  },
-  map: {
-    flex: 1,
-  },
+  container: { flex: 1 },
+  map: { flex: 1 },
 });
